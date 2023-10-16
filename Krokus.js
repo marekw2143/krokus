25abcf3edbe1979efca435dbfc73e1679bf0edaa
Marek Wawrzyczek <mwawrzyczek@gmail.com>
2023-10-16 17:05:35 +0200

    wip

--- Krokus.js
+++ Krokus.js
@@ -8,38 +8,42 @@ include("scripts/File/File.js");
 
 
 
-function CLOG(prefix){
-  function DO_LOG(text){
-    qDebug( text );
-    EAction.handleUserMessage( text );
-  };
+function CLOG(prefix)
+{
+    function DO_LOG(text)
+    {
+        qDebug( text );
+        EAction.handleUserMessage( text );
+    };
   
-  return function(text){
-    DO_LOG("Krokus::" + prefix + ": " + text);
-  };
+    return function(text)
+    {
+        DO_LOG("Krokus::" + prefix + ": " + text);
+    };
 }
 
 Krokus.prototype = new EAction();
 Krokus.includeBasePath = includeBasePath;
 
-function Krokus(guiAction) {
-  EAction.call(this, guiAction);
-  this.setUiOptions("Krokus.ui");
-  this.filesNumber = undefined;
+function Krokus(guiAction) 
+{
+    EAction.call(this, guiAction);
+    this.setUiOptions("Krokus.ui");
+    this.filesNumber = undefined;
 };
 
 Krokus.prototype.slotLiczbaPlikowChanged = function( v )
 {
-  CLOG("eventWrapper")("called with " + v);
+    CLOG("eventWrapper")("called with " + v);
 
-  this.filesNumber = new Number(v);
+    this.filesNumber = new Number(v);
 };
 
 Krokus.prototype.slotFileNamePatternChanged = function( v )
 {
-  CLOG("fileNameChanged")("set to: " + v);
+    CLOG("fileNameChanged")("set to: " + v);
 
-  this.fileNamePattern = v;
+    this.fileNamePattern = v;
 }
 
 Krokus.prototype.slotWykonajAkcje = function(v)
@@ -52,45 +56,48 @@ Krokus.prototype.slotWykonajAkcje = function(v)
   var version = "2010";
   var di = EAction.getDocumentInterface();
 
-  for(var currentFileNumber = 1; currentFileNumber <= this.filesNumber; currentFileNumber++)
-  {
-    var name = this.fileNamePattern + "_" + currentFileNumber + "." + extension;
+    for(var currentFileNumber = 1; currentFileNumber <= this.filesNumber; currentFileNumber++)
+    {
+        var name = this.fileNamePattern + "_" + currentFileNumber + "." + extension;
 
-    var fileName = di.getCorrectedFileName(name, version);
+        var fileName = di.getCorrectedFileName(name, version);
 
-    var doc = createOffScreenDocument();
+        var doc = createOffScreenDocument();
     
-    this.draw(doc, currentFileNumber);
+        this.draw(doc, currentFileNumber);
 
-    // export the document to a DXF file:
-    var newFileDi = new RDocumentInterface(doc);
+        // export the document to a DXF file:
+        var newFileDi = new RDocumentInterface(doc);
 
-    logger("saving file: " + fileName);
-    newFileDi.exportFile(fileName);
-  }
+        logger("saving file: " + fileName);
+        newFileDi.exportFile(fileName);
+    }
 };
 
 Krokus.prototype.draw = function(doc, fileNumber)
 {
-  var logger = CLOG("Draw");
+    var logger = CLOG("Draw");
 
-  logger("start, file number: " + fileNumber);
+    logger("start, file number: " + fileNumber);
 
-  startTransaction(doc);
+    startTransaction(doc);
 
-  this.doDraw( fileNumber );
+    this.doDraw( fileNumber );
 
-  endTransaction();
+    endTransaction();
 };
 
 Krokus.prototype.doDraw = function( drawingNumber )
 {
-  var x = 100 + drawingNumber;
-  var y = 100 - drawingNumber;
+    var x = 100 + drawingNumber;
+    var y = 100 - drawingNumber;
 
-  addLine(drawingNumber,drawingNumber, x, y);
+    addLine(drawingNumber,drawingNumber, x, y);
 }
 
 Krokus.prototype.beginEvent = function() {
+    var logger = CLOG("beginEvent");
+
+    logger("wywołano");
 };
 
