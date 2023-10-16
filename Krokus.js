6cc8eff888125dab1765343a9885d177af1e8f9e
Marek Wawrzyczek <mwawrzyczek@gmail.com>
2023-10-16 09:26:47 +0200

    wip

--- Krokus.js
+++ Krokus.js
@@ -5,6 +5,7 @@ include("scripts/sprintf.js");
 include("scripts/simple.js");
 include("scripts/WidgetFactory.js");
 include("scripts/File/File.js");
+
 function DO_LOG(text){
   qDebug( text );
   EAction.handleUserMessage( text );
@@ -56,9 +57,10 @@ Krokus.prototype.slotWykonajAkcje = function(v)
     var fileName = di.getCorrectedFileName(name, version);
 
     var doc = createOffScreenDocument();
-    startTransaction(doc);
+    
     this.draw(doc, i);
-    endTransaction();
+
+
     // export the document to a DXF file:
     var di = new RDocumentInterface(doc);
 
@@ -73,14 +75,20 @@ Krokus.prototype.draw = function(doc, i)
 
   logger("start, file number: " + i);
 
+  startTransaction(doc);
 
-// add something to the document:
-  var x = 100 + i;
-  var y = 100 - i;
+  this.doDraw( i );
 
-  addLine(0,0, x, y);
+  endTransaction();
 };
 
+Krokus.prototype.doDraw = function( drawingNumber )
+{
+  var x = 100 + drawingNumber;
+  var y = 100 - drawingNumber;
+
+  addLine(drawingNumber,drawingNumber, x, y);
+}
 
 Krokus.prototype.writeFiles(filesNumber) = function(filesNumber) {
 
@@ -88,23 +96,5 @@ Krokus.prototype.writeFiles(filesNumber) = function(filesNumber) {
 Krokus.prototype.beginEvent = function() {
   var dbg = CLOG("beginEvent");
   dbg("start");
-
-  // EAction.prototype.beginEvent.call(this);
-
-  // debugger;
-  // this.getProperties();
-  
-
-
-	// var doc = EAction.getDocument();
-
-	// startTransaction(doc);
-
-  // for(var i = 0; i < 100; i += 12){
-  //   var x = 100 + i;
-  //   var y = 100 - i;
-	//   addLine(0,0, x, y);
-  // }
-	// endTransaction();
 };
 
